5db8b9be1d51394625ebb2566ab7fa290aa224c4
Jose Varghese <jv595759@gmail.com>
2021-10-31 23:32:16 +0530

    Added config files and updated the script

--- k8s.sh
+++ k8s.sh
@@ -3,10 +3,9 @@ clear
 
 style_banner() {
 
-echo "--------------------------------------------------------------------------------------------"
+echo
 printf "%*s\n" $(( (${#1} + 90) / 2)) "$1"
-echo "--------------------------------------------------------------------------------------------"
-
+echo -e "********************************************************************************************\n"
 }
 
 print_op () {
@@ -51,18 +50,15 @@ fi
 }
 
 
-print_op "Loading config file"
-source ./k8s.conf >/dev/null 2>&1
-rc_stats
-
 #Cleaning
-style_banner "Cleaning all the lxc containers and proflies"
+style_banner "Cleaning lxc containers and proflies"
 
 print_op "Delete lxc containers"
 lxc delete --force $(lxc list -c n --format=csv) >/dev/null 2>&1
 rc_stats
 
 print_op "Delete profiles"
+lxc profile delete k8s >/dev/null 2>&1 && \
 lxc profile delete k8s-master >/dev/null 2>&1 && \
 lxc profile delete k8s-worker >/dev/null 2>&1
 rc_stats_w 
@@ -70,13 +66,20 @@ rc_stats_w
 #Base image setup
 style_banner "Creating base image"
 
+print_op "Loading config files"
+source ./k8s.conf >/dev/null 2>&1 && \
+ls ./haproxy.cfg >/dev/null 2>&1 && \
+lxc profile create k8s >/dev/null 2>&1 && \
+lxc profile edit k8s < ./k8s-profile
+rc_stats
+
 print_op "Set base profile"
 lxc profile set k8s limits.cpu 2 >/dev/null 2>&1 && \
 lxc profile set k8s limits.memory 4GB >/dev/null 2>&1
 rc_stats
 
 print_op "Launch base image"
-lxc launch ubuntu k8s -p k8s >/dev/null 2>&1
+lxc launch ubuntu:20.04 k8s -p k8s >/dev/null 2>&1
 rc_stats 
 
 #Installing packages
@@ -98,6 +101,7 @@ curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.
 echo \"deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main\" > /etc/apt/sources.list.d/kubernetes.list && \
 apt update -qq && \
 apt install -qq -y kubeadm=$k8s_version kubelet=$k8s_version kubectl=$k8s_version && \
+kubeadm config images pull && \
 echo 'KUBELET_EXTRA_ARGS=\"--fail-swap-on=false\"' > /etc/default/kubelet" >/dev/null 2>&1
 rc_stats
 
@@ -105,9 +109,95 @@ rc_stats
 
 if [[ $number_of_master_nodes -gt 1 ]]
 then
-	echo "Creating multi master cluster"
-	echo "Yet to enable this feature. exiting..."
-	exit 0
+	style_banner "Creating multi master cluster"
+	print_op "Creating master profile"
+	echo "127.0.0.1  localhost" > dns-list 2>/dev/null && \
+	echo "$load_balancer_ip  api-server" >> dns-list 2>/dev/null && \
+	lxc profile copy k8s k8s-master >/dev/null 2>&1 && \
+    lxc profile set k8s-master limits.cpu $master_node_cpu >/dev/null 2>&1 && \
+    lxc profile set k8s-master limits.memory $master_node_memory >/dev/null 2>&1
+	rc_stats 
+
+	count=0
+	while [[ $count -ne $number_of_master_nodes ]]
+	do
+	  	print_op "Launch master node kmaster$count"
+	  	lxc copy k8s "kmaster$count" -p k8s-master >/dev/null 2>&1 && \
+	   	lxc start "kmaster$count"
+	   	rc_stats
+
+	   	print_op "kmaster$count network status" 
+	    until [[ $(lxc list "kmaster$count" -c 4 --format=csv) ]]
+	    do
+	      sleep 5
+	    done
+	    rc_stats
+
+	   	let "count=count+1" 
+	done
+
+	print_op "Updating hosts file"
+	lxc list kmaster --format=json| jq -r '.[]|"\(.name) \(.state.network.eth0.addresses[0].address)"' >> dns-list 2>/dev/null && \
+	sed -i "/kmaster/c\\" /etc/hosts 2>/dev/null && \
+	sed -i "/api-server/c\\" /etc/hosts 2>/dev/null && \
+	sed -i "/localhost/c\\" /etc/hosts 2>/dev/null && \
+    cat dns-list >> /etc/hosts 2>/dev/null
+    rc_stats
+
+	count=0
+	while [[ $count -ne $number_of_master_nodes ]]
+	do
+	  	print_op "Updating kmaster$count DNS records"
+        lxc file push dns-list "kmaster$count"/etc/hosts >/dev/null 2>&1 
+	   	rc_stats
+
+	   	let "count=count+1" 
+	done
+
+    print_op "Updating haproxy conf"
+    cp ./haproxy.cfg /etc/haproxy/haproxy.cfg 2>/dev/null && \
+    sed -i "/kmaster/c\\" /etc/haproxy/haproxy.cfg 2>/dev/null && \
+    sed -i "/localhost/c\\" /etc/haproxy/haproxy.cfg 2>/dev/null && \
+	sed -i "/$load_balancer_ip/c\\" /etc/haproxy/haproxy.cfg 2>/dev/null && \
+    while read -r line
+    do
+    	echo "       server $line:6443 check" >> /etc/haproxy/haproxy.cfg 2>/dev/null
+    done < dns-list && \
+    systemctl restart haproxy >/dev/null 2>&1 
+    rc_stats
+
+	print_op "Bootstrap kmaster0 node"
+    lxc exec kmaster0 -- bash -c "mknod /dev/kmsg c 1 11 && \
+	echo 'mknod /dev/kmsg c 1 11' >> /etc/rc.local && \
+	chmod +x /etc/rc.local && \
+	kubeadm init --pod-network-cidr=10.244.0.0/16 --control-plane-endpoint $load_balancer_dns:$load_balancer_port --upload-certs --ignore-preflight-errors=all && \
+	mkdir /root/.kube && cp /etc/kubernetes/admin.conf /root/.kube/config && \
+	kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml" >/dev/null 2>&1
+	rc_stats
+
+	print_op "Get joining token"
+	masterJoinCommand="$(lxc exec kmaster0 -- sh -c 'kubeadm token create --print-join-command 2>/dev/null') --control-plane --certificate-key $(lxc exec kmaster0 -- sh -c 'kubeadm init phase upload-certs --upload-certs --one-output 2>/dev/null | grep -v upload-certs') --ignore-preflight-errors=all"
+	joinCommand="$(lxc exec kmaster0 -- sh -c 'kubeadm token create --print-join-command 2>/dev/null') --ignore-preflight-errors=all"
+	rc_stats	
+
+	print_op "Pull config file"
+	lxc file pull kmaster0/root/.kube/config ~/.kube/config >/dev/null 2>&1
+	rc_stats
+
+	count=1
+	while [[ $count -ne $number_of_master_nodes ]]
+	do
+	  	print_op "Bootstrap kmaster$count"
+        lxc exec "kmaster$count" -- bash -c "mknod /dev/kmsg c 1 11 && \
+	    echo 'mknod /dev/kmsg c 1 11' >> /etc/rc.local && \
+	    chmod +x /etc/rc.local && \
+	    eval $masterJoinCommand" >/dev/null 2>&1
+	    rc_stats
+
+	   	let "count=count+1" 
+	done
+
+
 elif [[ $number_of_master_nodes -eq 1 ]]
 then
 	style_banner "Creating single master cluster"
@@ -174,6 +264,10 @@ do
       sleep 5
     done
     rc_stats
+    
+    print_op "Add DNS in kworker$count"
+    lxc exec "kworker$count" -- bash -c "echo $load_balancer_ip  $load_balancer_dns >> /etc/hosts" >/dev/null 2>&1
+    rc_stats
 
     print_op "Bootstrap kworker$count"
     lxc exec "kworker$count" -- bash -c "mknod /dev/kmsg c 1 11 && \
@@ -185,4 +279,6 @@ do
    	let "count=count+1" 
 done
 
+print_op "Delete base Container"
 lxc delete k8s --force
+rc_stats_w
